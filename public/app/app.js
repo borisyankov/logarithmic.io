cf3fe80a8f45ad3737642851f44f99d8ffe78760
Boris Yankov <borisyankov@gmail.com>
2013-07-06 13:37:44 +0300

    Basic data storage and base of validation

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,16 @@
 function LoginController($scope) {
+
+    $scope.canLogin = function() {
+
+    }
+
     $scope.login = function() {
-        console.log($scope.loginForm.$valid);
+        console.log($scope.loginForm);
     }
 }
 
 function SignupController($scope) {
     $scope.signup = function() {
-        console.log($scope.signupForm.$valid);
+        console.log($scope.signupForm);
     }
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ var express = require('express'),
     server = module.exports = express();
 
 require('./env.js')(server, express);
-//require('./routes.js')(server);
+require('./data')();
 
 server.listen(process.env.PORT || 1337);
