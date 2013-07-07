fa6f59f81a64af190d376ed6c4a74b3e8aa065a2
Boris Yankov <borisyankov@gmail.com>
2013-07-08 00:41:55 +0300

    Add validation directives

--- public/app/directives.js
+++ public/app/directives.js
@@ -1,9 +1,37 @@
-angular.module('logarithmic').directive('error', function() {
-    return {
-        restrict: 'E',
-        replace: true,
-        transclude: true,
-        scope: { yo: '@', if: '&' },
-        template: '<div class="error" ng-show="if()" ng-transclude></div>'
-    };
-});
+angular.module('logarithmic')
+    .directive('error', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            scope: { yo: '@', if: '&' },
+            template: '<div class="error" ng-show="if()" ng-transclude></div>'
+        };
+    })
+    .directive('validIf', function($parse) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, elem, attrs, ctrl) {
+                scope.$watch(attrs.ngModel, function() {
+                    var valid = $parse(attrs.validIf)(scope);
+                    ctrl.$setValidity(attrs.ngModel, valid);
+                });
+
+            }
+        };
+    })
+    .directive('validIfEquals', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, elem, attrs, ctrl) {
+                scope.$watch(attrs.ngModel, function() {
+                    var v1 = scope[attrs.ngModel],
+                        v2 = scope[attrs.validIfEquals];
+                    ctrl.$setValidity(attrs.ngModel, angular.equals(v1, v2));
+                });
+            }
+        };
+    });
+
