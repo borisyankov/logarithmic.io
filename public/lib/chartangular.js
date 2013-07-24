ea950fc3010574b152422bd1b0a51ff165d552ca
Boris Yankov <borisyankov@gmail.com>
2013-07-25 01:54:17 +0300

    Update font file

---
+++ public/lib/chartangular.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function() {
+
+    var chartangular = angular.module('chartangular', []);
+
+    chartangular.directive('line-chart', function() {
+
+    });
+
+    chartangular.directive('bar-chart', function() {
+
+    });
+
+    chartangular.directive('pie-chart', function() {
+
+    });
+
+
+    chartangular.directive('doughnut-chart', function() {
+
+    });
+})();
